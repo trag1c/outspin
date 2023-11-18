afa1476afd53f6c33f32e75e4458c9248f627ecb
trag1c <trag1cdev@yahoo.com>
2023-11-18 21:57:13 +0100

    Consolidated the lib to one file

--- src/outspin/__init__.py
+++ src/outspin/__init__.py
@@ -1,3 +1,73 @@
-from .core import get_key, pause, wait_for
+from __future__ import annotations
 
-__all__ = ("get_key", "pause", "wait_for")
+import os
+import termios
+import tty
+
+
+def getch() -> str:
+    old_state = termios.tcgetattr(1)
+    tty.setcbreak(1)
+    try:
+        return os.read(1, 8).decode("utf-8")
+    finally:
+        termios.tcsetattr(1, termios.TCSADRAIN, old_state)
+
+
+MODS = {
+    "\x1b": "esc",
+    "\x1b[A": "up",
+    "\x1b[B": "down",
+    "\x1b[C": "right",
+    "\x1b[D": "left",
+    " ": "space",
+    "\t": "tab",
+    "\r": "enter",
+    "\n": "enter",
+    "\r\n": "enter",
+    "\x1bOP": "f1",
+    "\x1bOQ": "f2",
+    "\x1bOR": "f3",
+    "\x1bOS": "f4",
+    "\x1b[15~": "f5",
+    "\x1b[17~": "f6",
+    "\x1b[18~": "f7",
+    "\x1b[19~": "f8",
+    "\x1b[20~": "f9",
+    "\x1b[21~": "f10",
+    "\x1b[23~": "f11",
+    "\x1b[24~": "f12",
+    "\x1b[1;2A": "shift+up",
+    "\x1b[1;2B": "shift+down",
+    "\x1b[1;2C": "shift+right",
+    "\x1b[1;2D": "shift+left",
+    "\x1b\x1b[A": "alt+up",
+    "\x1b\x1b[B": "alt+down",
+    "\x1b\x1b[C": "alt+right",
+    "\x1b\x1b[D": "alt+left",
+    "\x1b[1;4A": "shift+alt+up",
+    "\x1b[1;4B": "shift+alt+down",
+    "\x1b[1;4C": "shift+alt+right",
+    "\x1b[1;4D": "shift+alt+left",
+    "\x0c": "^L",
+    "\x04": "^D",
+    "\x03": "^C",
+}
+
+
+def get_key() -> str:
+    return MODS.get(ch := getch(), ch)
+
+
+def wait_for(*keys: str) -> str:
+    while (key := get_key()) not in keys:
+        pass
+    return key
+
+
+def pause(prompt: str | None = None) -> None:
+    if prompt is None:
+        prompt = "Press any key to continue..."
+    print(prompt, end="", flush=True)
+    getch()
+    print()
