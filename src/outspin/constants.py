fe7c9f917cc875bb89e7b022fe772de3bab713ab
trag1c <trag1cdev@yahoo.com>
2023-11-21 22:31:16 +0100

    Added constants

---
+++ src/outspin/constants.py
@@ -0,0 +1,9 @@
+import string as _string
+
+ARROWS = ("up", "down", "left", "right")
+F_KEYS = tuple(f"f{i}" for i in range(1, 13))
+LOWERCASE = _string.ascii_lowercase
+UPPERCASE = _string.ascii_uppercase
+NUMBERS = DIGITS = _string.digits
+LETTERS = _string.ascii_letters
+PUNCTUATION = _string.punctuation
